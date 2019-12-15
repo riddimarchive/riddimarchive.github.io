31455583e6a6f54dc719c2a752ab3c934cb5c4b0
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2019-12-14 17:29:23 -0800

    Starting Node.js/Express Process
    Added index.js, ran local server on port 8080 successfully

---
+++ index.js
@@ -0,0 +1,28 @@
+// import express (after npm install express)
+const express = require('express');
+const fs = require('fs');
+
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+const PORT = 8080;
+
+// create a route for the app
+app.get('/', (req, res) => {
+  	res.writeHead(200, { 'Content-Type': 'text/html'});
+  	fs.readFile('index.html', function(error, data){
+  		if(error){
+  			res.writeHead(404);
+  			res.write('File No Found Yo')
+  		}else{
+  			res.write(data);
+  		}
+  		res.end();
+  	});
+});
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});

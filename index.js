00c38cd101aca43c58e14d16b98e64fbf0ceba60
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2019-12-14 20:02:57 -0800

    reverting index

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html'});
-    fs.readFile('index.html', function(error, data){
-      if(error){
-        res.writeHead(404);
-        res.write('File No Found Yo')
-      }else{
-        res.write(data);
-      }
-      res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-
-
-
-
-/*
 // import express (after npm install express)
 const express = require('express');
 const fs = require('fs');

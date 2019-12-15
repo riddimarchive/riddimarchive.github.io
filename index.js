9b4b9037fe5b2bfa5bc93b58408763128cc0e587
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2019-12-14 20:24:50 -0800

    index debug statements

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const PORT = 8080;
 
 // create a route for the app
 app.use(express.static('public'));
+console.log("public acquired");
 app.get('/', (req, res) => {
   	res.writeHead(200, { 'Content-Type': 'text/html'});
   	fs.readFile('index.html', function(error, data){
   		if(error){
   			res.writeHead(404);
-  			res.write('File No Found Yo')
+  			res.write('File No Found Yo');
   		}else{
+        console.log("WRITING DATA YO");
   			res.write(data);
   		}
   		res.end();

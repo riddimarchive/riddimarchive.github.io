e182d1acd65d547c7f313d3d7cb56e1ab5b492b2
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-01-04 11:47:13 -0800

    testing for compiling errors

--- index.js
+++ index.js
@@ -33,24 +33,7 @@ app.get('/', (req, res) => {
 
 
 
-const connecty = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
-connecty.connect((err) => {
-  if(err){
-    console.log('ERROR COULD NOT CONNECT NERD');
-    return;
-  }
-  console.log('Connected');
-});
-
-connecty.end((err) =>{
-  console.log('cant end connecty');
-});
 
 
 //end copy

11abf3d012b0c704d7353784cc4a739e852df3e8
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-01-25 13:05:29 -0800

    adding adduser, addtrack, and addartist queries

--- js/makequery.js
+++ js/makequery.js
@@ -68,6 +68,63 @@ function getAllTracksFromArtist(db, artist_name){
 	return querypromise;
 }
 
+function addUser(db, user){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`INSERT INTO users (id, access_level, username, password) VALUES (${user.id}, ${user.access_level}, ${user.username}, ${user.password})`, (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+
+	});
+
+	return querypromise;
+}
+
+function addTrack(db, track){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`INSERT INTO tracks (id, artist_id, artist_name, track_name, collab_artist, collab_artist2, collab_artist3, year, drive_url) VALUES (${track.id}, ${track.artist_id}, ${track.artist_name}, ${track.track_name}, ${track.collab_artist}, ${track.collab_artist2}, ${track.collab_artist3}, ${track.year}, ${track.drive_url})`, (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+
+	});
+
+	return querypromise;
+}
+
+function addArtist(db, artist){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`INSERT INTO users (id, artist_name, crew, country, info) VALUES (${artist.id}, ${artist.artist_name}, ${artist.crew}, ${artist.country}, ${artist.info})`, (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+
+	});
+
+	return querypromise;
+}
+
 
 
 module.exports = {

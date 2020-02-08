e4308c8766fd777cb280bdd7b3da24b6aa8abc58
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-02-08 11:30:35 -0800

    Update passport.js

--- js/passport.js
+++ js/passport.js
@@ -54,7 +54,7 @@ module.exports = function(passport){
 
 
 	passport.serializeUser((user, done) => {
-  		done(null, user.id);
+  		done(null, user);
 	});
 
 	passport.deserializeUser((id, done) => {

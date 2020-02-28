cfa81f41f0c3138096ae99496a8a38b6353155c6
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-02-27 20:36:46 -0800

    m

--- index.js
+++ index.js
@@ -698,7 +698,7 @@ app.post('/search', (req, res, next) => {
       }
 
   }else{
-    //search_results = SqlString.escape(search_results);
+    search_results = SqlString.escape(search_results);
     async function searchArtist(search_results, search_style){
         try{
             var artists = [];

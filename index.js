addf27f7f24376576860512c0dff2a0578280af6
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-02-27 20:21:10 -0800

    set up test for sqlescape

--- index.js
+++ index.js
@@ -678,8 +678,10 @@ app.post('/artistdelete', (req, res, next) => {
 app.post('/search', (req, res, next) => {
 
   var { search_results, search_style } = req.body;
+  search_results = SqlString.escape(search_results);
+  res.send(search_results);
 
-  if(!search_results || !search_style){
+  /*if(!search_results || !search_style){
 
       console.log("***a field is empty")
       if(!search_style){
@@ -696,7 +698,7 @@ app.post('/search', (req, res, next) => {
       }
 
   }else{
-    search_results = SqlString.escape(search_results);
+    //search_results = SqlString.escape(search_results);
     async function searchArtist(search_results, search_style){
         try{
             var artists = [];
@@ -775,7 +777,7 @@ app.post('/search', (req, res, next) => {
     searchArtist(search_results, search_style);
 
   }
-
+*/
 });
 
 

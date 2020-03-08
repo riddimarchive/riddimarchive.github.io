128071b96c121daec64c2cd8f81bae4b387bed2e
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-03-07 18:25:41 -0800

    Update email.js

--- js/email.js
+++ js/email.js
@@ -12,6 +12,7 @@ function storeArtistImage(artist_image, artist_name){
                 console.error('File couldnt be uploaded');
                 reject(err);
             }
+            console.log("file moved");
             resolve("File Stored!");
           });
 	});
@@ -31,35 +32,35 @@ function emailArtistForm(artist_name, crew, country, info, link, artist_img){
             Artist Info: ${info}
             Artist Link: ${link}`;
         
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                  user: process.env.EMAIL, 
-                  pass: process.env.EMAIL_PASSWORD
-                }
-            });
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.EMAIL, 
+              pass: process.env.EMAIL_PASSWORD
+            }
+        });
         
-            let mailOptions = {
-                from: process.env.EMAIL,
-                to: process.env.EMAIL,
-                subject: 'Artist Self-Submission',
-                text: output,
-                attachments: [
-                  {
-                      filename: `${artist_name}.jpg`,
-                      path: `../public/Images/Logos/${artist_name}.jpg` // stream this file
-                  }
-                ]
-            };
-        
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error){
-                    console.error('Email could not be sent');
-                    reject(error);
-                }
-                console.log("Email sent: %s", info.messageId);
-                resolve("Email Sent!");
-              });        
+        let mailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,
+            subject: 'Artist Self-Submission',
+            text: output,
+            attachments: [
+              {
+                  filename: `${artist_name}.jpg`,
+                  path: `../public/Images/Logos/${artist_name}.jpg` // stream this file
+              }
+            ]
+        };
+        console.log("sending email");
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error){
+                console.error('Email could not be sent');
+                reject(error);
+            }
+            console.log("Email sent: %s", info.messageId);
+            resolve("Email Sent!");
+          });        
 	});
 
 	return filepromise;

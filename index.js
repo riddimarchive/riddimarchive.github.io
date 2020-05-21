9a52e0c692cb504b58dc049f49d5a6c61886cee0
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-05-20 19:20:09 -0700

    testing page links

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.get('/', (req, res) => {
           }
         }
 
-        let tresult = await artquerie.getAllArtistsUthroughZ(db);
+        let tresult = await artquerie.getAllArtistsAthroughD(db);
 
         //store results
         for (var i = 0; i < tresult.length; i++) {

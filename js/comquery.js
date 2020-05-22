b1e8459031253e62a463d7625aeec1f02a64cf13
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-05-22 12:35:38 -0700

    Comment Display implemented - To do - Modal Scroll

--- js/comquery.js
+++ js/comquery.js
@@ -1,6 +1,6 @@
 function getAllCommentsByArtistName(db, artist_name){
 	let querypromise = new Promise(function(resolve, reject){
-		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time FROM comments INNER JOIN artists ON artists.id = comments.artist_id WHERE artists.artist_name = ?`, [artist_name], (error, result, fields) => {
+		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time FROM comments INNER JOIN artists ON artists.id = comments.artist_id WHERE artists.artist_name = ? ORDER BY comments.time`, [artist_name], (error, result, fields) => {
 	    	if (error) {
 	      		console.error('An error occurred while executing the query');
 	      		reject(error);
@@ -17,7 +17,7 @@ function getAllCommentsByArtistName(db, artist_name){
 
 function getAllCommentsByTrack(db, artist_name){
 	let querypromise = new Promise(function(resolve, reject){
-		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time, tracks.id FROM comments INNER JOIN tracks ON tracks.id = comments.track_id WHERE tracks.artist_name = ?`, [artist_name], (error, result, fields) => {
+		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time, tracks.id FROM comments INNER JOIN tracks ON tracks.id = comments.track_id WHERE tracks.artist_name = ? ORDER BY comments.time`, [artist_name], (error, result, fields) => {
 	    	if (error) {
 	      		console.error('An error occurred while executing the query');
 	      		reject(error);
@@ -34,7 +34,7 @@ function getAllCommentsByTrack(db, artist_name){
 
 function getAllCommentsByTrackName(db, track_name){
 	let querypromise = new Promise(function(resolve, reject){
-		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time, tracks.id FROM comments INNER JOIN tracks ON tracks.id = comments.track_id WHERE tracks.track_name = ?`, [track_name], (error, result, fields) => {
+		db.query(`SELECT comments.comment, comments.track_id, comments.user_id, comments.username, comments.time, tracks.id FROM comments INNER JOIN tracks ON tracks.id = comments.track_id WHERE tracks.track_name = ? ORDER BY comments.time`, [track_name], (error, result, fields) => {
 	    	if (error) {
 	      		console.error('An error occurred while executing the query');
 	      		reject(error);

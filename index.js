d5ebaa72cb3035076b4556194f124bdedae7a7ff
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-06-07 15:11:14 -0700

    Adding Google Drive API Test, Work in progress

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 flash = require('connect-flash');
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
 
 //file reqs: database connect, query functions, hash functions
 const createConnection = require('./js/dbconnect');
@@ -168,6 +171,84 @@ app.get('/faq', (req, res) => {
     theusername = req.user.username;
   }
   res.render('faq',{ title:'Riddim Archive FAQ', theusername: theusername  });
+
+});
+
+app.get('/googleapisuccess', (req, res) => {
+  res.send("It workeded?");
+});
+
+//GET REQUEST - GOOGLE DRIVE API TEST
+app.get('/googleapitest', (req, res) => {
+
+  const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+  const TOKEN_PATH = 'token.json';
+
+function authorize(credentials, callback) {
+  console.log("Calling Auth");
+  const {client_secret, client_id} = credentials.web;
+  var redirect_uris = [`https://www.riddimarchive.com/googleapitest`]
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function listFiles(auth) {
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const files = res.data.files;
+    if (files.length) {
+      console.log('Files:');
+      files.map((file) => {
+        console.log(`${file.name} (${file.id})`);
+      });
+    } else {
+      console.log('No files found.');
+    }
+  });
+}
+
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  console.log("FILE READ");
+  authorize(JSON.parse(content), listFiles);
+});
 
 });
 

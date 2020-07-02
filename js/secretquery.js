5d5b01e0a6b6cc3b6c815f5b1de3843a7c718cac
riddimarchive <58280354+riddimarchive@users.noreply.github.com>
2020-07-02 16:12:42 -0700

    Secret Hub - Info gets acquired correctly
    TODO: Display this Info For User, Generate Links and Change Password Buttons

--- js/secretquery.js
+++ js/secretquery.js
@@ -12,11 +12,84 @@ function getSecretLink(db, track_id){
 		});
 
 	});
-
 	return querypromise;
-
 }
 
+function getArtistsSecretLinks(db, artist_name){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`SELECT * FROM secretlinks WHERE artist_name = ?`, [artist_name], (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+	});
+	return querypromise;
+}
+
+function getArtistsSecretTunes(db, artist_name){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`SELECT * FROM tracks WHERE artist_name = ? AND is_secret = '1'`, [artist_name], (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+	});
+	return querypromise;
+}
+
+function addSecretLink(db, track_id, artist_name, url, exp_time){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`INSERT INTO secretlinks (track_id, artist_name, url, exp_time) 
+		VALUES (?, ?, ?, ?)`, [track_id, artist_name, url, exp_time], (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+	});
+	return querypromise;
+}
+
+function changeSecretPass(db, track_id, new_pass){
+
+	let querypromise = new Promise(function(resolve, reject){
+		db.query(`UPDATE tracks SET secret_pass = ? WHERE track_id = ?`, [new_pass, track_id], (error, result, fields) => {
+	    	if (error) {
+	      		console.error('An error occurred while executing the query');
+	      		reject(error);
+	    	}
+
+	    	resolve(result);
+
+		});
+
+	});
+	return querypromise;
+}
+
+
+
 module.exports = {
-	getSecretLink
+	getSecretLink,
+	getArtistsSecretLinks,
+	getArtistsSecretTunes,
+	addSecretLink,
+	changeSecretPass
 };
